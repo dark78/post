610eb0c5395b075f28b7c6a66ccd1e31512c2a2d
Anton Priadko <d.ark.3.1415@gmail.com>
2013-07-07 23:27:45 +0300

    FIrst model created

--- Post/settings.py
+++ Post/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'post.sqlite3',                      # Or path to database file if using sqlite3.
+        'NAME': '..\post.sqlite3',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'api'
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+
+class Journal(models.Model):
+	index = models.IntegerField(max_length=100)
+	name = models.CharField(max_length=100)
+	number = models.IntegerField()
+	price = models.FloatField()
+	publisher = models.ForeignKey(Publisher)
+
+class Subscriber(models.Model):
+	name = models.CharField(max_length=100)
+	address = models.CharField(max_length=100)
+	journal = models.ManyToManyField(Journal)
